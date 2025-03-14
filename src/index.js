0cc7b5bb8345363d4aa33ea2e4103dfb93f00a98
Spartiegirl <42212891+Spartiegirl@users.noreply.github.com>
2025-03-14 12:05:36 -0400

    basic js, added typewriter plugin

---
+++ src/index.js
@@ -0,0 +1,18 @@
+function generateRecipe(event) {
+    event.preventDefault();
+
+
+    new Typewriter("#recipe", {
+      strings: "Beef and Broccli",
+      autoStart: true,
+      delay: 1,
+      cursor: ""
+    });
+    recipeElement.innerHTML = "Beef and Broccli";
+
+}
+
+
+
+let recipeFormElement = document.querySelector("#recipe-generator");
+recipeFormElement.addEventListener("submit", generateRecipe);
